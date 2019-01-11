03cd7fff2c5e538507e338aded849b431fb35d8e
Amit Mishra <dev.amitkmishra@gmail.com>
2019-01-11 19:43:05 +0530

    checkoutsummary added

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 import Aux from './hoc/_aux';
 import Layout from './components/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
-
+import CheckoutSummary from './containers/Checkout/Checkout';
 class App extends Component {
   render() {
     return (
       <Aux>
         <Layout>
           <BurgerBuilder />
+          <CheckoutSummary />
         </Layout>
       </Aux>
     );
